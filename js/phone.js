79f74dc35b0c129cdc911ab415d248444a7731ac
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-03-01 17:38:28 +0600

    Card Title and Card Image is Dynamic now

--- js/phone.js
+++ js/phone.js
@@ -2,13 +2,35 @@
 const loadPhone = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    console.log(data.data);
-    loadPhones2(data.data);
+    const phones = data.data;
+    displayPhones(phones);
 }
- loadPhone();
 
- function loadPhones2(data){
-    for(const phone of data){
-        console.log(phone);
-    }
- }
+const displayPhones = phones => {
+        // Step-1 get container-div that where you set
+        const phonesContainer = document.getElementById('phones-container');
+
+        phones.forEach(phone => {
+            console.log(phone.phone_name);
+            // Step-2 create a div 
+            const phoneCards = document.createElement('div');
+            // set class attributes
+            phoneCards.classList = `card bg-base-100 p-4 shadow-xl`;
+            //Step-3 set inner html
+            phoneCards.innerHTML = `
+            <figure><img src="${phone.image}" alt="Phone" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">${phone.phone_name}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            `
+            // Step-4
+            phonesContainer.appendChild(phoneCards);
+        })
+}
+
+loadPhone();
+
