564d6f73564c0df2744278c43beba60d6a122cc3
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-03-02 23:15:30 +0600

    Show loading spiner is added

--- js/phone.js
+++ js/phone.js
@@ -9,7 +9,16 @@ const loadPhone = async(searchText) => {
 const displayPhones = phones => {
         // Step-1 get container-div that where you set
         const phonesContainer = document.getElementById('phones-container');
-
+        // remove phones content before added
+        phonesContainer.textContent = '';
+        const showAll = document.getElementById('show-all-container');
+        if(phones.length > 12){
+            showAll.classList.remove('hidden');
+        }
+        else{
+            showAll.classList.add('hidden');
+        }
+        phones = phones.slice(0, 12);
         phones.forEach(phone => {
             // Step-2 create a div 
             const phoneCards = document.createElement('div');
@@ -21,23 +30,36 @@ const displayPhones = phones => {
                 <div class="card-body">
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                    <div class="card-actions justify-center">
+                        <button class="btn btn-primary">Show Details</button>
                     </div>
                 </div>
             `
             // Step-4
             phonesContainer.appendChild(phoneCards);
-        })
+        });
+
+        toggleLoading(false);
 }
 
 
 // handle search button
 const handleSearch = () => {
+    toggleLoading(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhone(searchText);
 }
 
+// Loading handler  
+const toggleLoading = (isLoading) => {
+    const loadingContainer = document.getElementById('loading-container');
+    if(isLoading){
+        loadingContainer.classList.remove('hidden');
+    }
+    else{
+        loadingContainer.classList.add('hidden');
+    }
+}
 // loadPhone();
 
